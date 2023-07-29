d970f4570b9f71bedb21480e191357af20dea4ae
Derek Santolo <derek.santolo@gmail.com>
2023-07-29 16:24:48 -0500

    - add SvelteKit - add initial App component and styling

---
+++ src/components/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "./App.scss";
+</script>
+
+<main class="main">
+  <div class="main-foreground">
+    <div class="top-panel" />
+    <h1 class="name">Derek Santolo</h1>
+    <div class="bottom-panel" />
+  </div>
+  <div class="main-background"><h1 class="hidden">Main Underground</h1></div>
+</main>
