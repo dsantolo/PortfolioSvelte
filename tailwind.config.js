dd83be1fb866c8409672648f4f338cdd18929395
Derek Santolo <derek.santolo@gmail.com>
2023-10-08 21:18:43 -0500

    chore(): push missing Tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeout: {
+          '0%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+      animation: {
+        fadeout: 'fadeout both',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animated')],
+};
