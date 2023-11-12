02455ae773e028c010875bc27786f90ed9da75d2
Derek Santolo <derek.santolo@gmail.com>
2023-11-11 21:03:02 -0600

    - add NameContainer component

---
+++ src/components/NameContainer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let open = false;
+</script>
+
+{#key open}
+  <div
+    class={`animate-fade animate-duration-500 animate-ease-in ${
+      open ? 'animate-reverse' : ''
+    }`}
+  >
+    <div
+      class={`name:name-container font-comfortaa via-12% via-12% flex h-[5vw] w-screen flex-col items-center justify-center bg-gradient-to-l from-red-700 from-5% via-gray-300 to-slate-700 to-95% bg-[percentage:500%] text-5xl font-thin hover:animate-fade-right hover:bg-gradient-to-r hover:from-black hover:via-slate-700 hover:to-white hover:bg-cover hover:text-white hover:animate-normal hover:animate-duration-500 hover:animate-ease-in ${
+        open ? '' : 'animate-gradientflow'
+      }`}
+      role="button"
+      tabindex="0"
+    >
+      <h1>Derek Santolo</h1>
+    </div>
+  </div>
+{/key}
