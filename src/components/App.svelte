8bdcfd9e2b5e973d4e772ce591fd59db5ee4f059
Derek Santolo <derek.santolo@gmail.com>
2023-11-18 19:56:20 -0600

    - Refactor conditional styling from template strings to classnames NPM module - Fix bug where NameContainer was still above MainBackground on z-axis when opened

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import cn from 'classnames';
   import MainBackground from './MainBackground.svelte';
   import NameContainer from './NameContainer.svelte';
   import { onMount } from 'svelte';
@@ -36,26 +37,32 @@
 </script>
 
 <main
-  class={`grid h-auto w-screen grid-cols-1 grid-rows-1 overflow-hidden ${
-    open ? '-z-10' : 'max-h-screen md:max-h-none'
-  }`}
+  class={cn(
+    'grid h-auto  w-screen grid-cols-1 grid-rows-1 overflow-hidden ',
+    { '-z-10': open },
+    { 'max-h-screen md:max-h-none': !open },
+  )}
 >
   <MainBackground {open} />
   <div
     class="main-foreground top-0 col-span-1 col-start-1 row-span-1 row-start-1 m-0 flex h-screen w-screen flex-col items-center justify-center"
   >
     <div
-      class={`top-panel relative h-[47.5vh] w-screen border-b-2 border-solid border-gray-500 bg-black transition-transform ${
-        open ? 'translate-x-[-100vw] duration-1000 ease-linear' : ''
-      }`}
+      class={cn(
+        'top-panel relative h-[47.5vh] w-screen border-b-2 border-solid border-gray-500 bg-black transition-transform',
+        {
+          'translate-x-[-100vw] duration-1000 ease-linear': open,
+        },
+      )}
       class:open
       bind:this={topPanel}
     />
     <NameContainer {open} />
     <div
-      class={`bottom-panel relative h-[47.5vh] w-screen border-t-2 border-solid border-gray-500 bg-black transition-transform ${
-        open ? 'translate-x-[100vw] duration-1000 ease-linear' : ''
-      }`}
+      class={cn(
+        'bottom-panel relative h-[47.5vh] w-screen border-t-2 border-solid border-gray-500 bg-black transition-transform',
+        { 'translate-x-[100vw] duration-1000 ease-linear': open },
+      )}
       class:open
     />
   </div>
--- src/components/NameContainer.svelte
+++ src/components/NameContainer.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+  import cn from 'classnames';
   export let open = false;
 </script>
 
 {#key open}
   <div
-    class={`animate-fade animate-duration-[250ms] ${
-      open ? 'animate-reverse' : ''
-    }`}
+    class={cn('animate-fade animate-duration-[250ms]', {
+      '-z-10 animate-reverse': open,
+    })}
   >
     <div
-      class={`name:name-container font-comfortaa via-12% via-12% flex h-[7vh] w-screen flex-col items-center justify-center bg-gradient-to-l from-red-700 from-5% via-gray-300 to-slate-700 to-95% bg-[percentage:500%] text-2xl font-thin hover:animate-fade-right hover:bg-gradient-to-r hover:from-black hover:via-slate-700 hover:to-white hover:bg-cover hover:text-white hover:animate-normal hover:animate-duration-500 hover:animate-ease-in sm:text-4xl ${
-        open ? '' : 'animate-gradientflow'
-      }`}
+      class={cn(
+        'name:name-container font-comfortaa via-12% via-12% flex h-[7vh] w-screen flex-col items-center justify-center bg-gradient-to-l from-red-700 from-5% via-gray-300 to-slate-700 to-95% bg-[percentage:500%] text-2xl font-thin hover:animate-fade-right hover:bg-gradient-to-r hover:from-black hover:via-slate-700 hover:to-white hover:bg-cover hover:text-white hover:animate-normal hover:animate-duration-500 hover:animate-ease-in sm:text-4xl',
+        {
+          'animate-gradientflow': !open,
+        },
+      )}
       role="button"
       tabindex="0"
     >
